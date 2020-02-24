dcc67413935b99f531d9023e76acb482de91c8f6
Michael <Michael_veilis@hotmail.com>
2020-02-24 21:54:19 +0100

    C#/.NET projekt med FileIO

---
+++ C#.NETProjects/FileIO/FileIO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This is a FileWriter and FileReader project");
+        }
+    }
+}
