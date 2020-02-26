c769b55f528a50a154e4e78b49aed3980d60df81
Michael <Michael_veilis@hotmail.com>
2020-02-26 16:29:55 +0100

    Added start of MirrorTree project for java

---
+++ JavaProjects/MirrorTree/src/main/java/dk/klmm/mirrortree/Main.java
@@ -0,0 +1,8 @@
+package dk.klmm.mirrortree;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Execute the conversion of tree to it's mirror state here!");
+    }
+}
